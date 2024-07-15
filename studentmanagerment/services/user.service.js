241c7f417bd98b64a912c6618373dfb543306083
sixgillkrahs <dvq2804@gmail.com>
2024-07-15 23:42:45 +0700

    config cors

--- studentmanagerment/services/user.service.js
+++ studentmanagerment/services/user.service.js
@@ -19,6 +19,20 @@ module.exports = {
 			"numberphone",
 			"role",
 		],
+		cors: {
+			// Configures the Access-Control-Allow-Origin CORS header.
+			origin: "*",
+			// Configures the Access-Control-Allow-Methods CORS header.
+			methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+			// Configures the Access-Control-Allow-Headers CORS header.
+			allowedHeaders: [],
+			// Configures the Access-Control-Expose-Headers CORS header.
+			exposedHeaders: [],
+			// Configures the Access-Control-Allow-Credentials CORS header.
+			credentials: false,
+			// Configures the Access-Control-Max-Age CORS header.
+			maxAge: 3600,
+		},
 		entityValidator: {
 			username: { type: "string", min: 6 },
 			password: { type: "string", min: 8 },
